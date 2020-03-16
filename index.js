27d3a8945c7d069b7d9e1631717ecf4cc176f487
e <veronikamail.21@gmail.com>
2020-03-16 20:03:10 +0300

    c

--- index.js
+++ index.js
@@ -6,31 +6,26 @@ const options = {
 const bot = new TelegramBot(TOKEN, options);
 
 
-var res1;
+
 
 
 var r = 'start' ;
-console.log(r);
+
 var jsforce = require('jsforce');
 var conn = new jsforce.Connection();
 conn.login('expenseapplication@sccraft.com', 'asdfg123', function(err, res) {
-  r = '1';
-  console.log('in login');
+
   if (err) { return console.error(err); }
   conn.query('SELECT Id, Name FROM Account LIMIT 1', function(err, res) {
-    r = '2';
-    console.log('in query');
     if (err) { return console.error(err); }
     r = res.records[0].Name;
-    res1 = res;
-    console.log(r);
   });
 });
-console.log('hi');
+
 bot.on('message', msg => {
   bot.sendMessage(msg.chat.id, `Veronika klubnika ${msg.from.first_name} bla bla ${r}`);
 }) 
-r = '3';
+
 
 
 /*var respon1;
