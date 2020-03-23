87c801a1fda8463e48ce4f0f79b9c94a20f13df6
e <veronikamail.21@gmail.com>
2020-03-23 12:10:48 +0300

    upd

--- index.js
+++ index.js
@@ -116,9 +116,9 @@ bot.on('callback_query', callbackQuery => {
 
     }
     conn.sobject("Expense_Card__c").create({ 
-      Card_Keeper__c : contactId,
-      Card_Date__c : cardDate,
-      Amount__c : 10
+      'Card_Keeper__c' : contactId,
+      'Card_Date__c' : cardDate,
+      'Amount__c' : 10
     }, function(err, ret) {
       if (err || !ret.success) { return console.error(err, ret); }
       console.log("Created record id : " + ret.id);
