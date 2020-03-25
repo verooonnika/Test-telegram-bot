dc847c240c17e43cb8ecd6e3266c54c317525c03
e <veronikamail.21@gmail.com>
2020-03-25 19:35:24 +0300

    update

--- index.js
+++ index.js
@@ -10,114 +10,76 @@ var contactId = '';
 var jsforce = require('jsforce');
 var conn = new jsforce.Connection();
 
+var chatId;
+
+var todayDay = new Date();
+var month = todayDay.getMonth();
+var monthName;
+var year = todayDay.getFullYear();
+
+var changeMessageId;
+
+var previousMessage;
+
+var amount;
+var description;
+
+var dayToInsert;
+
+var login;
+
 bot.onText(/\/start/, msg => {
+
   conn.login('expenseapplication@sccraft.com', 'asdfg123', function(err, res) {
 
-    if (err) { return console.error(err); }
+    if (err) {  
+       bot.sendMessage(msg.chat.id, 'Ошибка авторизации в Salesforce');
+     return console.error(err); }
     else {
-      bot.sendMessage(msg.chat.id, 'Введите логин: ');
-      bot.onText(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, msg => {
-        var login = msg.text;
-        console.log(login);
-        bot.sendMessage(msg.chat.id, 'Введите пароль: ');
-        bot.on('message', msg =>{
-          var password = msg.text;
-          conn.query(
-            "SELECT Id, Name, Email FROM Contact " +
-            "WHERE Email = '" + login + "' "  +
-            "AND Password__c = '" + password + "' "  +
-            "LIMIT 1", function (err, res) {
-              if (err) {  bot.sendMessage(msg.chat.id, 'Invalid login or password ');
-              return console.error('err', err); 
-            }
-            contactId = res.records[0].Id;
-            console.log('contactId' + contactId);
-
-            const opts = {
-              reply_markup: JSON.stringify({
-                inline_keyboard: [
-                  [{"text":"Текущий баланс","callback_data":"balance"},
-                  {"text":"Создать карточку","callback_data":"new-card"}]
-                ] 
-              })
-          };
-      
-          bot.sendMessage(msg.chat.id, 'Авторизация прошла успешно!', opts); 
-
-          bot.on('callback_query', callbackQuery => {
-            var answer = callbackQuery.data;
-            const msg = callbackQuery.message;
-            console.log(answer);
-            if(answer == 'new-card'){
-              const opts = {
-                reply_markup: JSON.stringify({
-                  inline_keyboard: [
-                    [{"text":"Сегодня","callback_data":"today"},
-                    {"text":"Календарь","callback_data":"calendar"},
-                    {"text":"Отмена","callback_data":"cancel"}]
-                  ] 
-                })
-            };
-            bot.sendMessage(msg.chat.id, 'На какой день желаете создать карточку?', opts); 
-
-            bot.on('callback_query', callbackQuery => {
-              var answer = callbackQuery.data;
-              var cardDate;
-              var amount;
-              var description;
-              if(answer == 'today'){
-                console.log(answer);
-                cardDate = new Date('2020-03-19');
-              } else if(answer == 'calendar'){
-                console.log(answer);
-          
-              } else if(answer == 'cancel'){
-                console.log(answer);
-          
-              }
-              console.log(contactId);
-              console.log(cardDate);
-
-              var fl = 0;
-              fl = 1;
-            bot.sendMessage(msg.chat.id, 'Введите сумму: ');
-            bot.on('message', msg => {
-              amount = msg.text; 
-             
-              if(fl == 1){
-                bot.sendMessage(msg.chat.id, 'Введите описание: ');
-                bot.on('message', msg => {
-                  description = msg.text
-                  bot.sendMessage(msg.chat.id, 'Вы ввели: ' + amount + description);
-                  fl = 2;                
-                })
-              }
-
-
-            })
-
-            })
-
-            } else if (answer == 'balance'){
-              console.log(answer);
-            }
-
-          })
-           // return contactId;
-          })
-        })
-
-
-      })
+      bot.sendMessage(msg.chat.id, 'Введите логин: ').then(previousMessage = 'login');
     }
   });
 })
 
-/*bot.on('callback_query', callbackQuery => {
+function mainMenu(chatId){
+
+  const opts = {
+    reply_markup: JSON.stringify({
+      inline_keyboard: [
+        [{"text":"Текущий баланс","callback_data":"balance"},
+        {"text":"Создать карточку","callback_data":"new-card"}]
+      ] 
+    })
+};
+bot.sendMessage(chatId, 'Выберете действие: ', opts); 
+
+}
+
+function insertCard(cardDate, chatId){
+
+bot.sendMessage(chatId, 'Введите сумму: ').then(previousMessage = 'amount');
+}
+
+function getBalance(chatId){
+  conn.query(
+    "SELECT Reminder__c FROM Monthly_Expense__c " +
+    "WHERE Keeper__c = '" + contactId + "'", function (err, res) {
+      if (err) {  bot.sendMessage(msg.chat.id, 'Query error ');
+      return console.error('err', err); 
+    }
+    var balance = 0;
+    for(var i = 0; i < res.records.length; i++){
+      balance += res.records[i].Reminder__c;
+    }
+    balance = (Math.round(balance * 100) / 100).toFixed(2);
+    bot.sendMessage(chatId, 'Ваш баланс: ' + balance + '$'); 
+  })
+
+}
+
+bot.on('callback_query', callbackQuery => {
   var answer = callbackQuery.data;
-  const msg = callbackQuery.message;
   if(answer == 'new-card'){
- console.log(answer);
     const opts = {
       reply_markup: JSON.stringify({
         inline_keyboard: [
@@ -127,95 +89,195 @@ bot.onText(/\/start/, msg => {
         ] 
       })
   };
+  bot.sendMessage(chatId, 'На какой день желаете создать карточку?', opts); 
 
-  bot.sendMessage(msg.chat.id, 'На какой день желаете создать карточку?', opts); 
-
-  bot.on('callback_query', callbackQuery => {
-    var answer = callbackQuery.data;
-    var cardDate;
-    var amount;
-    var description;
-    if(answer == 'today'){
-      console.log(answer);
-      cardDate = new Date('2020-03-19');
-    } else if(answer == 'calendar'){
-      console.log(answer);
-
-    } else if(answer == 'cancel'){
-      console.log(answer);
-
-    }
-    console.log(contactId);
-    console.log(cardDate);
-
-
-    bot.sendMessage(msg.chat.id, 'Введите сумму: ');
-    bot.on('message', msg => {
-      amount = msg.text; 
-      console.log(amount);
-      bot.sendMessage(msg.chat.id, 'Введите описание: ');
-      bot.on('message', msg => {
-        description = msg.text;
-        console.log(amount);
-        console.log(description);
-
-
-        conn.sobject("Expense_Card__c").create({ 
-          Card_Keeper__c : contactId,
-          Card_Date__c : cardDate,
-          Amount__c : amount,
-          Description__c : description
-    
-        }, function(err, ret) {
-          if (err || !ret.success) { return console.error(err, ret); }
-          console.log("Created record id : " + ret.id + ret.Amount__c + ret.Description__c);
-          // ...
-        })
-      })
-      
-    })
-
-  })
+  }else if(answer == 'balance'){
+    getBalance(chatId);
+  }else if(answer == 'today'){
+    previousMessage = 'today';
+    dayToInsert = todayDay;
+    insertCard(todayDay, chatId);
+  }else if(answer == 'calendar'){
+   changeMonthName();
+   sendCalendar(chatId);
+  }else if(answer == 'cancel'){
+    mainMenu(chatId);
+  }else if(answer == '<'){
+    changeMessageId = callbackQuery.message.message_id;
+    previousMonth();
   }
-
-
+  else if(answer == '>'){
+    changeMessageId = callbackQuery.message.message_id;
+    nextMonth();
+  } else if(answer >= 1 && answer <= 31){
+    getDate(answer);
+  }
 });
 
-function trylogin(msg, login, password){
-  console.log('in trylogin' + login + password);
-  conn.query(
-    "SELECT Id, Name, Email FROM Contact " +
-    "WHERE Email = '" + login + "' "  +
-    "AND Password__c = '" + password + "' "  +
-    "LIMIT 1", function (err, res) {
-      if (err) {  bot.sendMessage(msg.chat.id, 'Invalid login or password ');
-      return console.error('err', err); 
+bot.on('message', msg => {
+
+  if(previousMessage == 'login'){
+    login = msg.text;
+    bot.sendMessage(msg.chat.id, 'Введите пароль: ').then(previousMessage = 'password');
+  } else if(previousMessage == 'password'){
+    var password = msg.text;
+    conn.query(
+      "SELECT Id, Name, Email FROM Contact " +
+      "WHERE Email = '" + login + "' "  +
+      "AND Password__c = '" + password + "' "  +
+      "LIMIT 1", function (err, res) {
+        if (err) { 
+        return console.error('err', err); 
+        
+      }
+      console.log(res);
+      if(res.records.length == 0){
+        bot.sendMessage(msg.chat.id, 'Invalid login or password ');
+        bot.sendMessage(msg.chat.id, 'Введите логин: ').then(previousMessage = 'login');
+      }
+
+      contactId = res.records[0].Id;
+      chatId = msg.chat.id;
+      bot.sendMessage(chatId, 'Авторизация прошла успешно!').then(mainMenu(chatId));   
+    })
+  } else if(previousMessage == 'amount'){
+    amount = msg.text;
+    bot.sendMessage(msg.chat.id, 'Введите описание: ').then(previousMessage = 'description');
+  } else if(previousMessage == 'description'){
+    description = msg.text;
+    cardDate = dayToInsert;
+    conn.sobject("Expense_Card__c").create({ 
+      Card_Keeper__c : contactId,
+      Card_Date__c : cardDate,
+      Amount__c : amount,
+      Description__c : description
+
+    }, function(err, ret) {
+      if (err || !ret.success) { return console.error(err, ret); }
+      bot.sendMessage(msg.chat.id, 'Карточка успешно добавлена! ').then(mainMenu(chatId));
+    })
+  }
+
+})
+
+function showCalendar(){
+
+  var numOfdays = getDays();
+  var inlineCalendar = [];
+
+  var topRow = [];
+
+  var buttonLeft = new Object();
+  buttonLeft.text = '<';
+  buttonLeft.callback_data = '<';
+  topRow.push(buttonLeft);
+
+  var buttonYear = new Object();
+  buttonYear.text = monthName + ' ' + year;
+  buttonYear.callback_data = year;
+  topRow.push(buttonYear);
+  
+  var buttonRight = new Object();
+  buttonRight.text = '>';
+  buttonRight.callback_data = '>';
+  topRow.push(buttonRight);
+
+  inlineCalendar.push(topRow);
+
+  var row = [];
+  for(var i = 1; i <= numOfdays; i++){
+
+    if(i % 7 == 0){
+      inlineCalendar.push(row);
+      row = [];
     }
 
-    contactId = res.records[0].Id;
-    console.log('contactId' + contactId);
-    return contactId;
-  })
-}
-
-
-*/
-
-/*var respon1;
-var q = 'SELECT Id, Name, FROM Account LIMIT 1';
-
-
-org.query({ query: q }, function(err, resp){
-respon1 = resp.records[0];
-  if(!err && resp.records) {
-
-    var acc = resp.records[0];
-    acc.set('Name', 'Really Spiffy Cleaners');
-    acc.set('Industry', 'Cleaners');
-
-    org.update({ sobject: acc, oauth: oauth }, function(err, resp){
-      if(!err) console.log('It worked!');
-    });
+      var button = new Object();
+      button.text = i.toString();
+      button.callback_data = i;
+      row.push(button);
 
   }
-});*/
+  inlineCalendar.push(row); 
+  return inlineCalendar;
+
+}
+
+function getDays(){
+  var numberOfMonths = 0;
+
+  if(month == 3 || month == 5 || month == 8 || month == 10){
+    numberOfMonths = 30;
+  } else if(month == 1){
+    numberOfMonths = 28;
+  } else{
+    numberOfMonths = 31;
+  }
+
+  return numberOfMonths;
+
+}
+
+function previousMonth(){
+  if(month == 0){
+    month = 11;
+    year-= 1;
+  }else{
+  month -= 1;
+  }
+  changeMonthName();
+  changeCalendar();
+}
+
+function nextMonth(){
+  if(month == 11){
+    month = 0;
+    year +=1;
+  }else{
+  month += 1;
+}
+  changeMonthName();
+  changeCalendar();
+}
+
+function sendCalendar(chatId){
+  var inlineCalendar = showCalendar();
+  const opts = {
+    reply_markup: JSON.stringify({
+      inline_keyboard: inlineCalendar 
+    })
+};
+   bot.sendMessage(chatId, 'Выберете дату: ', opts); 
+}
+
+function changeCalendar(){
+  var inlineCalendar = showCalendar();
+
+  const opts = {
+    chat_id: chatId,
+    message_id: changeMessageId,
+  };
+
+  bot.editMessageReplyMarkup( {
+    inline_keyboard: inlineCalendar 
+}, opts);
+}
+
+function changeMonthName(){
+  const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+  "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"
+];
+
+monthName = monthNames[month];
+}
+
+function getDate(answer){
+
+//var stringDate = year + '-' + month + '-' + answer;
+//console.log(stringDate);
+console.log(answer);
+var newDate = new Date(year, month, answer); ////////////////////////////////////////////////////////////////
+console.log(newDate);
+dayToInsert = newDate;
+insertCard(newDate, chatId);
+}
